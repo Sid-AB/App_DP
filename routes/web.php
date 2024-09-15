eb4db5c8771b2f4d515b28d4af6ca6677bf73adf
nourelhoudaas <sayahhouda06@gmail.com>
2024-09-15 15:34:18 +0100

    add

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,31 @@ Route::get('/testing/tree',function (){
 Route::controller(portfeuilleController::class)->group(function(){
     Route::get('/Portfail','index')->name('home.portfail');
 });
+
+//===============ROUTE PROGRAMME==============================
+Route::controller(programmeController::class)->group(function(){
+});
+
+//===============ROUTE SOUS PROGRAMME==============================
+Route::controller(sousProgrammeController::class)->group(function(){
+});
+
+//===============ROUTE ACTION==============================
+Route::controller(actionController::class)->group(function(){
+});
+
+//===============ROUTE SOUS ACTION==============================
+Route::controller(sousActionController::class)->group(function(){
+});
+
+//===============ROUTE GROUPE D'OPERATIONS==============================
+Route::controller(groupOperationController::class)->group(function(){
+});
+
+//===============ROUTE  OPERATION==============================
+Route::controller(operationController::class)->group(function(){
+});
+
+//===============ROUTE SOUS OPERATION==============================
+Route::controller(sousOperationController::class)->group(function(){
+});
