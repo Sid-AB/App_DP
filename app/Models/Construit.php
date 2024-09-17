7761c0633d50898037438c8c98aeabbad0f0381c
fadia-boumediene <fadiaboumediene11@gmail.com>
2024-09-17 15:38:14 +0100

    database

--- app/Models/Construit.php
+++ app/Models/Construit.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Construit extends Model
 {
     use HasFactory;
+    protected $table = 'construits';
+    protected $primaryKey = 'id_construit';
+    public $incrementing = false; 
+    protected $keyType = 'integer'; 
+    public $timestamps = false;
+
+    protected $fillable = [
+       'id_construit','id_extrait','id_rff','num_action'
+ ];
+   
+ 
+    public function RFF()
+    {
+        return $this->belongsTo(RFF::class);
+    }
+
+    public function Action()
+    {
+        return $this->belongsTo(Action::class);
+    }
+
+    public function Extrait_DPIC()
+    {
+        return $this->belongsTo(Extrait_DPIC::class);
+    }
 }
