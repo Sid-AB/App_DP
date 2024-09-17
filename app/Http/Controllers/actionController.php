2cecff4d60a1c23a3c706942fc09f4a3d8568828
nourelhoudaas <sayahhouda06@gmail.com>
2024-09-17 15:44:33 +0100

    add

--- app/Http/Controllers/actionController.php
+++ app/Http/Controllers/actionController.php
@@ -11,78 +11,99 @@ class actionController extends Controller
         return view('Action-in.index');
     }
 
-    public function insertDPA(Request $request, $t1, $t2, $t3, $t4)
+    public function insertDPA(Request $request, $t1, $t2, $t3, $t4, $sousAction)
 {   if($t1==1)
     {
-    $montants = $request->input('montants');
+    $montantAe = $request->input('montantAe');
+    $montantCp = $request->input('montantCp');
 
-    foreach ($montants as $code => $montant) {
-        // Vérifier si le code représente un groupe d'opération
+    foreach ($montantAe as $code => $montantAe) {
+        // Si le montant AE n'a pas été saisi, on lui attribue la valeur par défaut de 0.00
+        $montantCp = $montantCp ?? 0.00;
+
+        // Récupérer Montant CP pour chaque code, et s'il est vide, on lui attribue 0.00
+        $montantCp = $montantsCp[$code] ?? 0.00;
+
+        // Récupérer le nom associé à chaque code (optionnel si présent dans la requête)
+        $nom = $request->input("noms[$code]");
+
+        // Vérifier si le code représente un groupe d'opérations
         if ($code % 1000 == 0) {
-            // Insérer dans la table groupoperation
+            // Insertion dans la table groupoperation
             GroupOperation::updateOrCreate(
-                ['code_gp' => $code],
-                ['montant' => $montant]
+                ['code_grp_operation' => $code],
+                ['nom_grp_operation' => $nom, 'AE_grp_operation' => $montantAe, 'CP_grp_operation' => $montantCp]
             );
         }
         // Vérifier si le code représente une opération
         elseif ($code % 100 == 0) {
-            // Récupérer le code du groupe d'opération (par exemple 11000 pour le code 11100)
             $codeGp = floor($code / 1000) * 1000;
 
-            // Insérer dans la table operation
+            // Insertion dans la table operation
             Operation::updateOrCreate(
-                ['code_op' => $code],
-                ['code_gp' => $codeGp, 'montant' => $montant]
+                ['code_operation' => $code],
+                ['code_grp_operation' => $codeGp, 'nom_operation' => $nom, 'AE_operation' => $montantAe, 'CP_operation' => $montantCp]
             );
         }
         // Sinon, il s'agit d'une sous-opération
         else {
-            // Récupérer le code de l'opération parent (par exemple 11100 pour le code 11110)
             $codeOp = floor($code / 100) * 100;
 
-            // Insérer dans la table sousoperation
-            SousOperation::updateOrCreate(
-                ['code_sop' => $code],
-                ['code_op' => $codeOp, 'montant' => $montant]
+            // Insertion dans la table sousoperation
+            SubOperation::updateOrCreate(
+                ['code_sous_operation' => $code],
+                ['code_operation' => $codeOp, 'nom_sous_operation' => $nom, 'AE_sous_operation' => $montantAe, 'CP_sous_operation' => $montantCp]
             );
         }
     }
 
     return back()->with('success', 'Données insérées avec succès !');
-}elseif($t2==2)
-{
-    $montants = $request->input('montants');
 
-    foreach ($montants as $code => $montant) {
-        // Vérifier si le code représente un groupe d'opération
+}
+elseif($t2==2)
+{
+    $montantAe_ouvert = $request->input('montantAe_ouvert');
+    $montantCp_ouvert = $request->input('montantCp_ouvert');
+
+    $montantCp_atendu = $request->input('montantCp_atendu');
+    $montantCp_atendu = $request->input('montantCp_atendu');
+
+    foreach ($montantAe_ouvert as $code => $montantAe_ouvert) {
+        // Si le montant AE n'a pas été saisi, on lui attribue la valeur par défaut de 0.00
+        $montantCp_atendu = $montantCp_atendu ?? 0.00;
+
+        // Récupérer Montant CP pour chaque code, et s'il est vide, on lui attribue 0.00
+        $montantCp = $montantsCp[$code] ?? 0.00;
+
+        // Récupérer le nom associé à chaque code (optionnel si présent dans la requête)
+        $nom = $request->input("noms[$code]");
+
+        // Vérifier si le code représente un groupe d'opérations
         if ($code % 1000 == 0) {
-            // Insérer dans la table groupoperation
+            // Insertion dans la table groupoperation
             GroupOperation::updateOrCreate(
-                ['code_gp' => $code],
-                ['montant' => $montant]
+                ['code_grp_operation' => $code],
+                ['nom_grp_operation' => $nom, 'AE_grp_operation' => $montantAe, 'CP_grp_operation' => $montantCp]
             );
         }
         // Vérifier si le code représente une opération
         elseif ($code % 100 == 0) {
-            // Récupérer le code du groupe d'opération (par exemple 11000 pour le code 11100)
             $codeGp = floor($code / 1000) * 1000;
 
-            // Insérer dans la table operation
+            // Insertion dans la table operation
             Operation::updateOrCreate(
-                ['code_op' => $code],
-                ['code_gp' => $codeGp, 'montant' => $montant]
+                ['code_operation' => $code],
+                ['code_grp_operation' => $codeGp, 'nom_operation' => $nom, 'AE_operation' => $montantAe, 'CP_operation' => $montantCp]
             );
         }
         // Sinon, il s'agit d'une sous-opération
         else {
-            // Récupérer le code de l'opération parent (par exemple 11100 pour le code 11110)
             $codeOp = floor($code / 100) * 100;
 
-            // Insérer dans la table sousoperation
+            // Insertion dans la table sousoperation
             SubOperation::updateOrCreate(
-                ['code_sop' => $code],
-                ['code_op' => $codeOp, 'montant' => $montant]
+                ['code_sous_operation' => $code],
+                ['code_operation' => $codeOp, 'nom_sous_operation' => $nom, 'AE_sous_operation' => $montantAe, 'CP_sous_operation' => $montantCp]
             );
         }
     }
@@ -92,35 +113,42 @@ class actionController extends Controller
 }elseif ($t3==3) {
     $montants = $request->input('montants');
 
-    foreach ($montants as $code => $montant) {
-        // Vérifier si le code représente un groupe d'opération
+    foreach ($montantAe as $code => $montantAe) {
+        // Si le montant AE n'a pas été saisi, on lui attribue la valeur par défaut de 0.00
+        $montantAe = $montantAe ?? 0.00;
+
+        // Récupérer Montant CP pour chaque code, et s'il est vide, on lui attribue 0.00
+        $montantCp = $montantsCp[$code] ?? 0.00;
+
+        // Récupérer le nom associé à chaque code (optionnel si présent dans la requête)
+        $nom = $request->input("noms[$code]");
+
+        // Vérifier si le code représente un groupe d'opérations
         if ($code % 1000 == 0) {
-            // Insérer dans la table groupoperation
+            // Insertion dans la table groupoperation
             GroupOperation::updateOrCreate(
-                ['code_gp' => $code],
-                ['montant' => $montant]
+                ['code_grp_operation' => $code],
+                ['nom_grp_operation' => $nom, 'AE_grp_operation' => $montantAe, 'CP_grp_operation' => $montantCp]
             );
         }
         // Vérifier si le code représente une opération
         elseif ($code % 100 == 0) {
-            // Récupérer le code du groupe d'opération (par exemple 11000 pour le code 11100)
             $codeGp = floor($code / 1000) * 1000;
 
-            // Insérer dans la table operation
+            // Insertion dans la table operation
             Operation::updateOrCreate(
-                ['code_op' => $code],
-                ['code_gp' => $codeGp, 'montant' => $montant]
+                ['code_operation' => $code],
+                ['code_grp_operation' => $codeGp, 'nom_operation' => $nom, 'AE_operation' => $montantAe, 'CP_operation' => $montantCp]
             );
         }
         // Sinon, il s'agit d'une sous-opération
         else {
-            // Récupérer le code de l'opération parent (par exemple 11100 pour le code 11110)
             $codeOp = floor($code / 100) * 100;
 
-            // Insérer dans la table sousoperation
+            // Insertion dans la table sousoperation
             SubOperation::updateOrCreate(
-                ['code_sop' => $code],
-                ['code_op' => $codeOp, 'montant' => $montant]
+                ['code_sous_operation' => $code],
+                ['code_operation' => $codeOp, 'nom_sous_operation' => $nom, 'AE_sous_operation' => $montantAe, 'CP_sous_operation' => $montantCp]
             );
         }
     }
@@ -129,35 +157,42 @@ class actionController extends Controller
 }elseif (t4==4) {
     $montants = $request->input('montants');
 
-    foreach ($montants as $code => $montant) {
-        // Vérifier si le code représente un groupe d'opération
+    foreach ($montantAe as $code => $montantAe) {
+        // Si le montant AE n'a pas été saisi, on lui attribue la valeur par défaut de 0.00
+        $montantAe = $montantAe ?? 0.00;
+
+        // Récupérer Montant CP pour chaque code, et s'il est vide, on lui attribue 0.00
+        $montantCp = $montantsCp[$code] ?? 0.00;
+
+        // Récupérer le nom associé à chaque code (optionnel si présent dans la requête)
+        $nom = $request->input("noms[$code]");
+
+        // Vérifier si le code représente un groupe d'opérations
         if ($code % 1000 == 0) {
-            // Insérer dans la table groupoperation
+            // Insertion dans la table groupoperation
             GroupOperation::updateOrCreate(
-                ['code_gp' => $code],
-                ['montant' => $montant]
+                ['code_grp_operation' => $code],
+                ['nom_grp_operation' => $nom, 'AE_grp_operation' => $montantAe, 'CP_grp_operation' => $montantCp]
             );
         }
         // Vérifier si le code représente une opération
         elseif ($code % 100 == 0) {
-            // Récupérer le code du groupe d'opération (par exemple 11000 pour le code 11100)
             $codeGp = floor($code / 1000) * 1000;
 
-            // Insérer dans la table operation
+            // Insertion dans la table operation
             Operation::updateOrCreate(
-                ['code_op' => $code],
-                ['code_gp' => $codeGp, 'montant' => $montant]
+                ['code_operation' => $code],
+                ['code_grp_operation' => $codeGp, 'nom_operation' => $nom, 'AE_operation' => $montantAe, 'CP_operation' => $montantCp]
             );
         }
         // Sinon, il s'agit d'une sous-opération
         else {
-            // Récupérer le code de l'opération parent (par exemple 11100 pour le code 11110)
             $codeOp = floor($code / 100) * 100;
 
-            // Insérer dans la table sousoperation
+            // Insertion dans la table sousoperation
             SubOperation::updateOrCreate(
-                ['code_sop' => $code],
-                ['code_op' => $codeOp, 'montant' => $montant]
+                ['code_sous_operation' => $code],
+                ['code_operation' => $codeOp, 'nom_sous_operation' => $nom, 'AE_sous_operation' => $montantAe, 'CP_sous_operation' => $montantCp]
             );
         }
     }
