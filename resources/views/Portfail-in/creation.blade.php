5948d93a1d7721bab4bcd815e8d1da3721196e41
sid-ab <ayoub.06.08.10@gmail.com>
2024-11-20 14:52:16 +0100

    update

--- resources/views/Portfail-in/creation.blade.php
+++ resources/views/Portfail-in/creation.blade.php
@@ -73,11 +73,10 @@
                         <label for="inputFile">Journal scanner</label>
                         <label for="pdf_file">Choisissez un fichier PDF :</label>
                         <input type="file" name="pdf_file" id="pdf_file" accept=".pdf, .jpg, .jpeg, .png">
-                        <button id="pdf-upload-form" data-id="1">Télécharger le PDF</button>
-
                     </div>
 
                 </form>
+                <br>
                 <button type="submit" class="btn btn-primary" id="add-wallet">
                     <i class="fas fa-plus"></i> Ajouter
                 </button>
