fe3d98be473450001f56f1c11f7b12a5e3c27261
sid-ab <=>
2025-01-30 15:09:07 +0100

    update

--- app/Http/Controllers/portfeuilleController.php
+++ app/Http/Controllers/portfeuilleController.php
@@ -429,7 +429,7 @@ public function live_File($id)
             ]);
         }
         {
-        $file=DB::table('multimedia')->where('related_id',$id)->select('filepath')->orderBy('date_upload')->first();
+        $file=DB::table('multimedia')->where('related_id',$id)->select('filepath')->orderBy('date_upload','desc')->first();
         if(!isset($file))
         {
             abort(404);
