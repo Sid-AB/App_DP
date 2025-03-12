2c682f070ee5a66dc901d9d9f8469b3f988f3071
sid-ab <=>
2025-03-12 14:49:05 +0100

    update

--- resources/views/Admin/index.blade.php
+++ resources/views/Admin/index.blade.php
@@ -31,8 +31,82 @@
 
       <div style="display: flex;align-items: center;justify-content: center;flex-direction: row;flex-wrap: wrap;">
        <div class="card align-items-center" style="width: 60%">
-         <div class="row align-items-center" style="width: 150px">
-          <img src="{{asset('assets/img/logo_ministere.svg')}}" alt="">
+         <div class="row align-items-center"style="justify-content: center;">
+          <img src="{{asset('assets/img/logo_ministere.svg')}}" alt="" style="width: 60%"/>
+
+
+          <div >
+          <form class="row g-3 needs-validation" style="padding: 5px 10px 15px 10px; " enctype="multipart/form-data"  novalidate>
+            <div class="col-md-3">
+              <label for="validationCustom01" class="form-label">Nom</label>
+              <input type="text" class="form-control" id="validationCustom01" value="Mark" required>
+              <div class="valid-feedback">
+              
+              </div>
+            </div>
+            <div class="col-md-3">
+              <label for="validationCustom02" class="form-label">Prenom</label>
+              <input type="text" class="form-control" id="validationCustom02" value="Otto" required>
+              <div class="valid-feedback">
+               
+              </div>
+            </div>
+            <div class="col-md-6">
+              <label for="validationCustomUsername" class="form-label">Local Email</label>
+              <div class="input-group has-validation">
+                <input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
+                <span class="input-group-text" id="inputGroupPrepend">@mcomm.local</span>
+               
+                <div class="invalid-feedback" >
+                 
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <label for="validationCustom03" class="form-label">Sous Direction</label>
+              <select class="form-select" id="validationCustom04" required>
+                <option selected disabled value="">Choisir...</option>
+                <option>...</option>
+              </select>
+              <div class="invalid-feedback">
+                Please provide a valid city.
+              </div>
+            </div>
+            <div class="col-md-3">
+              <label for="validationCustom04" class="form-label">Previlaige</label>
+              <select class="form-select" id="validationCustom04" required>
+                <option selected disabled value="">Choose...</option>
+                <option>...</option>
+              </select>
+              <div class="invalid-feedback">
+                
+              </div>
+            </div>
+            <div class="col-md-3">
+              <label for="validationCustom05" class="form-label">Le Code </label>
+              <input type="text" class="form-control" id="validationCustom05" required>
+              <div class="invalid-feedback">
+               
+              </div>
+            </div>
+            <div class="col-12">
+              <div class="form-check">
+                <input class="form-control" type="file" value="" id="file" required>
+                <label class="form-check-label" for="invalidCheck">
+                  Ajouter Decision
+                </label>
+                <div class="invalid-feedback">
+                  You must agree before submitting.
+                </div>
+              </div>
+            </div>
+            <div class="col-12">
+              <button class="btn btn-primary" type="submit">Submit form</button>
+            </div>
+          </form>
+          </div>
+
+
          </div>
         </div>
       </div>
@@ -72,224 +146,6 @@
 
           <!-- ========== tables-wrapper start ========== -->
           <div class="tables-wrapper">
-            <div class="row">
-              <div class="col-lg-12">
-                <div class="card-style mb-30">
-                  <h6 class="mb-10">Data Table</h6>
-                  <p class="text-sm mb-20">
-                    For basic styling—light padding and only horizontal
-                    dividers—use the class table.
-                  </p>
-                  <div class="table-wrapper table-responsive">
-                    <table class="table">
-                      <thead>
-                        <tr>
-                          <th class="lead-info">
-                            <h6>Lead</h6>
-                          </th>
-                          <th class="lead-email">
-                            <h6>Email</h6>
-                          </th>
-                          <th class="lead-phone">
-                            <h6>Phone No</h6>
-                          </th>
-                          <th class="lead-company">
-                            <h6>Company</h6>
-                          </th>
-                          <th>
-                            <h6>Action</h6>
-                          </th>
-                        </tr>
-                        <!-- end table row-->
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td class="min-width">
-                            <div class="lead">
-                              <div class="lead-image">
-                                <img src="assets/images/lead/lead-1.png" alt="" />
-                              </div>
-                              <div class="lead-text">
-                                <p>Courtney Henry</p>
-                              </div>
-                            </div>
-                          </td>
-                          <td class="min-width">
-                            <p><a href="#0">yourmail@gmail.com</a></p>
-                          </td>
-                          <td class="min-width">
-                            <p>(303)555 3343523</p>
-                          </td>
-                          <td class="min-width">
-                            <p>UIdeck digital agency</p>
-                          </td>
-                          <td>
-                            <div class="action">
-                              <button class="text-danger">
-                                <i class="lni lni-trash-can"></i>
-                              </button>
-                            </div>
-                          </td>
-                        </tr>
-                        <!-- end table row -->
-                        <tr>
-                          <td class="min-width">
-                            <div class="lead">
-                              <div class="lead-image">
-                                <img src="assets/images/lead/lead-2.png" alt="" />
-                              </div>
-                              <div class="lead-text">
-                                <p>John Doe</p>
-                              </div>
-                            </div>
-                          </td>
-                          <td class="min-width">
-                            <p><a href="#0">yourmail@gmail.com</a></p>
-                          </td>
-                          <td class="min-width">
-                            <p>(303)555 3343523</p>
-                          </td>
-                          <td class="min-width">
-                            <p>Graygrids digital agency</p>
-                          </td>
-                          <td>
-                            <div class="action">
-                              <button class="text-danger">
-                                <i class="lni lni-trash-can"></i>
-                              </button>
-                            </div>
-                          </td>
-                        </tr>
-                        <!-- end table row -->
-                        <tr>
-                          <td class="min-width">
-                            <div class="lead">
-                              <div class="lead-image">
-                                <img src="assets/images/lead/lead-3.png" alt="" />
-                              </div>
-                              <div class="lead-text">
-                                <p>David Smith</p>
-                              </div>
-                            </div>
-                          </td>
-                          <td class="min-width">
-                            <p><a href="#0">yourmail@gmail.com</a></p>
-                          </td>
-                          <td class="min-width">
-                            <p>(303)555 3343523</p>
-                          </td>
-                          <td class="min-width">
-                            <p>Abc agency</p>
-                          </td>
-                          <td>
-                            <div class="action">
-                              <button class="text-danger">
-                                <i class="lni lni-trash-can"></i>
-                              </button>
-                            </div>
-                          </td>
-                        </tr>
-                        <!-- end table row -->
-                        <tr>
-                          <td class="min-width">
-                            <div class="lead">
-                              <div class="lead-image">
-                                <img src="assets/images/lead/lead-4.png" alt="" />
-                              </div>
-                              <div class="lead-text">
-                                <p>Jonathon</p>
-                              </div>
-                            </div>
-                          </td>
-                          <td class="min-width">
-                            <p><a href="#0">yourmail@gmail.com</a></p>
-                          </td>
-                          <td class="min-width">
-                            <p>(303)555 3343523</p>
-                          </td>
-                          <td class="min-width">
-                            <p>Creative IT Agency</p>
-                          </td>
-                          <td>
-                            <div class="action">
-                              <button class="text-danger">
-                                <i class="lni lni-trash-can"></i>
-                              </button>
-                            </div>
-                          </td>
-                        </tr>
-                        <!-- end table row -->
-                        <tr>
-                          <td>
-                            <div class="lead">
-                              <div class="lead-image">
-                                <img src="assets/images/lead/lead-5.png" alt="" />
-                              </div>
-                              <div class="lead-text">
-                                <p>Anna Lee</p>
-                              </div>
-                            </div>
-                          </td>
-                          <td>
-                            <p><a href="#0">yourmail@gmail.com</a></p>
-                          </td>
-                          <td>
-                            <p>(303)555 3343523</p>
-                          </td>
-                          <td>
-                            <p>Halal It agency</p>
-                          </td>
-                          <td>
-                            <div class="action">
-                              <button class="text-danger">
-                                <i class="lni lni-trash-can"></i>
-                              </button>
-                            </div>
-                          </td>
-                        </tr>
-                        <!-- end table row -->
-                        <tr>
-                          <td>
-                            <div class="lead">
-                              <div class="lead-image">
-                                <img src="assets/images/lead/lead-6.png" alt="" />
-                              </div>
-                              <div class="lead-text">
-                                <p>Gray Simon</p>
-                              </div>
-                            </div>
-                          </td>
-                          <td>
-                            <p><a href="#0">yourmail@gmail.com</a></p>
-                          </td>
-                          <td>
-                            <p>(303)555 3343523</p>
-                          </td>
-                          <td>
-                            <p>DesignCourse</p>
-                          </td>
-                          <td>
-                            <div class="action">
-                              <button class="text-danger">
-                                <i class="lni lni-trash-can"></i>
-                              </button>
-                            </div>
-                          </td>
-                        </tr>
-                        <!-- end table row -->
-                      </tbody>
-                    </table>
-                    <!-- end table -->
-                  </div>
-                </div>
-                <!-- end card -->
-              </div>
-              <!-- end col -->
-            </div>
-            <!-- end row -->
-
-            <!-- end row -->
-
             <div class="row">
               <div class="col-lg-12">
                 <div class="card-style mb-30">
