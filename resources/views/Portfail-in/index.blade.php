39c7f9e08ef0f2bd56d4c189217184800ce361d6
sid-ab <=>
2025-03-18 08:56:48 +0100

    'update'

--- resources/views/Portfail-in/index.blade.php
+++ resources/views/Portfail-in/index.blade.php
@@ -718,8 +718,10 @@ listItemsWithNestedUl.each(function(){
           $(".hide-access-form").removeClass('form-access');
           $('.hide-access-form').empty()
         })
+
+        window.location.href='/testing/Action/'+path3[0]+'/'+path3[1]+'/'+path3[2]+'/'+typeact[1]+'/?code='+response.account
         var cosnt=0;
-        $('#btn-form-access').on('click',function(){
+        /*$('#btn-form-access').on('click',function(){
           if(cosnt == 0) {
           $.ajax({
 
@@ -741,7 +743,7 @@ listItemsWithNestedUl.each(function(){
               $('#code_generated').css('border-color','red')
               console.log('out of range')
             }
-          })
+          })*/
           cosnt++;
          }
         })
