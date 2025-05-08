0d3e56061499bf5f9c126565f156f8698578d221
sid-ab <=>
2025-05-08 12:24:57 +0100

    update

--- resources/views/impression/impression_emplois_budgetaire.blade.php
+++ resources/views/impression/impression_emplois_budgetaire.blade.php
@@ -164,9 +164,9 @@
 <table>
     <tr>
         <th rowspan="2" class="mauve" style="width:36.3%;">OP + APPARITEURS + CONDUCTEURS</th>
-        <th rowspan="2" class="highlight-gray">{{ $totalOuvertscomm }}</th>
-        <th rowspan="2" class="highlight-gray">{{ $totalOccupescomm }}</th>
-        <th rowspan="2" class="highlight-gray">{{ $totalVacantscomm }}</th>
+        <th rowspan="2" class="highlight-gray" style="width:36.3%;">{{ $totalOuvertscomm }}</th>
+        <th rowspan="2" class="highlight-gray" style="width:36.3%;">{{ $totalOccupescomm }}</th>
+        <th rowspan="2" class="highlight-gray" style="width:36.3%;">{{ $totalVacantscomm }}</th>
         <th rowspan="2" class="header-row">CATEGORIE</th>
         <th rowspan="2" class="header-row">MOYENNE</th>
         <th rowspan="2" class="header-row">TRAITEMENT ANNUEL</th>
